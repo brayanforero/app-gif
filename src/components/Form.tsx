871e880b41f3c148fa8ce74ed1729fb2e4be7c09
Brayan Forero <bforero.engineer@gmail.com>
2022-06-24 11:52:04 -0400

    add form

---
+++ src/components/Form.tsx
@@ -0,0 +1,37 @@
+import { ChangeEvent, FormEvent, useCallback, useState } from 'react'
+import { useLocation } from 'wouter'
+import './Form.css'
+function Form() {
+  const [keyword, setKeyword] = useState<string>('')
+  const [, setLocation] = useLocation()
+  const handleSubmit = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault()
+
+      if (!keyword) {
+        return alert('Please, type something')
+      }
+
+      setLocation(`/search/${keyword}`)
+    },
+    [keyword]
+  )
+
+  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value)
+  }, [])
+
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <input
+        onChange={handleChange}
+        value={keyword}
+        className="form__input"
+        type="text"
+      />
+      <button className="form__submit">Search</button>
+    </form>
+  )
+}
+
+export default Form
