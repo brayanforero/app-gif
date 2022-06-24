9c573e76752683dfa7477b2947c92e4113298dc1
Brayan Forero <bforero.engineer@gmail.com>
2022-06-24 11:04:33 -0400

    add page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,17 @@
+import './index.css'
+import logo from './../../assets/img/logo.svg'
+import { Form } from '../../components'
 function Home() {
-  return <div>home</div>
+  return (
+    <section className="home">
+      <picture className="home__picture">
+        <img src={logo} alt="mopics brand logo" />
+      </picture>
+
+      <p className="home__subtitle">Explore a world of gifs</p>
+      <Form />
+    </section>
+  )
 }
 
 export default Home
