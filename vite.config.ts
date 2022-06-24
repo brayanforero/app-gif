9d15cdbe3faaa1acc45206f0e585e3b8d63fd597
Brayan Forero <bforero.engineer@gmail.com>
2022-06-23 20:15:07 -0400

    Revert "add alias import module"
    This reverts commit a9f2038a69cc0dab58561e60d9b8dc3181fa059f.

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@assets': './src/assets/',
-      '@layouts': './src/layouts/',
-      '@pages': './src/pages/',
-      '@components': './src/components/',
-      '@interfaces': './src/interfaces/',
-      '@hooks': './src/hooks/',
-      '@context': './src/context/',
-    },
-  },
+  plugins: [react()]
 })
