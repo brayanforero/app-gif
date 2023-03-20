54e758f8d505720c36c712c958857426ab4a7519
Brayan Forero <bforero.engineer@gmail.com>
2023-03-20 12:11:07 -0400

    ui(root): add scroll snap effect

---
+++ src/components/common/categories/Categories/index.tsx
@@ -0,0 +1,6 @@
+import "./index.css";
+function Categories() {
+  return <section className="Categories section">Categories</section>;
+}
+
+export default Categories;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
+import { Categories } from "@components/common/categories";
 import { HomeFeed } from "@components/common/home";
 
 function Home() {
   return (
     <>
       <HomeFeed />
+      <Categories />
     </>
   );
 }
