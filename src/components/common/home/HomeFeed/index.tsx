c6d89e3e71d5b0576286bdb49fc0d92a6ae66f57
Brayan Forero <bforero.engineer@gmail.com>
2023-03-20 11:48:13 -0400

    refactor(root) move and extract component to another component

---
+++ src/components/common/home/HomeFeed/index.tsx
@@ -0,0 +1,16 @@
+import Form from "@components/Form";
+import Logo from "@components/Logo";
+import TrendingTerms from "@components/TrendingTerms";
+import "./index.css";
+function HomeFeed() {
+  return (
+    <section className="Home">
+      <Logo size="small" />
+      <p className="Home__subtitle gradient-purple">Explore a world of gifs</p>
+      <Form />
+      <TrendingTerms />
+    </section>
+  );
+}
+
+export default HomeFeed;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,9 @@
-import { Form, Logo, TrendingTerms } from "../../components";
-import "./index.css";
-// TODO: Load all of categories by lazy load and handle error in its file
+import { HomeFeed } from "@components/common/home";
 
 function Home() {
   return (
     <>
-      <section className="Home">
-        <Logo size="small" />
-        <p className="Home__subtitle gradient-purple">
-          Explore a world of gifs
-        </p>
-        <Form />
-        <TrendingTerms />
-      </section>
+      <HomeFeed />
     </>
   );
 }
