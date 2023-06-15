e57acb99e00bbc95173f2e98a5c8b3bef5ec8ff0
Brayan Forero <bforero.engineer@gmail.com>
2023-06-15 15:37:14 -0400

    refactor(root): change loading size image in hook useCateories

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -1,44 +1,44 @@
-import { useEffect, useState } from 'react'
-import Category from '../interfaces/app/categories'
-import { getCategories } from '../services/categories'
+import { useEffect, useState } from "react";
+import Category from "../interfaces/app/categories";
+import { getCategories } from "../services/categories";
 
 function useCategories() {
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState<string | null>(null)
-  const [categories, setCategories] = useState<Category[]>([])
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [categories, setCategories] = useState<Category[]>([]);
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
 
     getCategories()
-      .then(res => {
-        if (!res) return
+      .then((res) => {
+        if (!res) return;
 
-        const categoriesFormated = res.data.map(c => {
-          const { name } = c
-          const { downsized } = c.gif.images
+        const categoriesFormated = res.data.map((c) => {
+          const { name } = c;
+          const { fixed_height_downsampled } = c.gif.images;
 
           return {
             name,
-            image: downsized.url,
-          }
-        })
+            image: fixed_height_downsampled.url,
+          };
+        });
 
-        setCategories(categoriesFormated)
+        setCategories(categoriesFormated);
       })
-      .catch(_err => {
-        setError('Fetching data error')
+      .catch((_err) => {
+        setError("Fetching data error");
       })
       .finally(() => {
-        setLoading(false)
-      })
-  }, [])
+        setLoading(false);
+      });
+  }, []);
 
   return {
     loading,
     categories,
     error,
-  }
+  };
 }
 
-export default useCategories
+export default useCategories;
