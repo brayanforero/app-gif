936cd6b4a3c7bd3358a17b194e19a7862555a0a2
Brayan Forero <bforero.engineer@gmail.com>
2023-06-15 17:48:53 -0400

    fix(root): update imports

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,25 +1,25 @@
-import { useCallback, useEffect } from 'react'
-import debounce from 'just-debounce-it'
-import { Link } from 'wouter'
-import { Form, Grid, Logo } from '../../components'
-import InfiniteScroll from '../../components/InfiniteScroll'
-import { useGifs } from './../../hooks'
-import './index.css'
-import { BallTriangle } from 'react-loader-spinner'
+import { useCallback, useEffect } from "react";
+import debounce from "just-debounce-it";
+import { Link } from "wouter";
+import { useGifs } from "./../../hooks";
+import "./index.css";
+import { BallTriangle } from "react-loader-spinner";
+import { Logo, Form, InfiniteScroll } from "@components/shared";
+import Grid from "@components/Grid";
 interface Props {
-  params: { keyword: string }
+  params: { keyword: string };
 }
 
 function Search({ params }: Props) {
-  const { gifs, loading, page, pages, setPage } = useGifs(params.keyword)
+  const { gifs, loading, page, pages, setPage } = useGifs(params.keyword);
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
   const handlerNextPage = useCallback(
-    debounce(() => setPage(prev => prev + 1), 800),
+    debounce(() => setPage((prev) => prev + 1), 800),
     []
-  )
+  );
   return (
     <section className="Search">
       <Link href="/" className="active">
@@ -36,14 +36,14 @@ function Search({ params }: Props) {
         dataLength={gifs.length}
         hasMore={pages > page}
         handlerEndSection={handlerNextPage}
-        margin={0}
+        threshold={0}
         isLoading={loading}
         loader={<BallTriangle color="#5747eb" height={80} width={80} />}
       >
         <Grid items={gifs} />
       </InfiniteScroll>
     </section>
-  )
+  );
 }
 
-export default Search
+export default Search;
