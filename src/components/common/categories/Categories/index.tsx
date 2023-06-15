1c41f3a5d5e8b40b63c98bbf65919753dc3365a8
Brayan Forero <bforero.engineer@gmail.com>
2023-06-15 16:02:07 -0400

    refactor(root): rewrite all structure of categories section

--- src/components/common/categories/Categories/index.tsx
+++ src/components/common/categories/Categories/index.tsx
@@ -1,6 +1,24 @@
+import useCategories from "@hooks/useCategories";
+import CategoryItem from "../Category";
 import "./index.css";
+
 function Categories() {
-  return <section className="Categories section">Categories</section>;
+  const { categories, error, loading } = useCategories();
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <div className="alert">Something has been wrong</div>;
+
+  return (
+    <section className="Categories">
+      <h2>Categories</h2>
+      <div className="grid">
+        {categories.map((item) => (
+          <CategoryItem key={`${item.name}`} item={item} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Categories;
