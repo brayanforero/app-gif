22ea0d6949c1a1fe6e855f6fb2e326a1d89691c6
Brayan Forero <bforero.engineer@gmail.com>
2023-06-15 15:48:49 -0400

    fix(root) remove wrong import from LazyImage file

--- src/components/shared/LazyImage/index.tsx
+++ src/components/shared/LazyImage/index.tsx
@@ -2,7 +2,6 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import "react-lazy-load-image-component/src/effects/black-and-white.css";
 import "react-lazy-load-image-component/src/effects/opacity.css";
-import "./index.css";
 type LazyImageCustomProp = Pick<
   HTMLImageElement,
   "src" | "alt" | "width" | "height"
