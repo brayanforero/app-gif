6c536ce6ccd16d311d5049a906ae8807b384cab4
Brayan Forero <bforero.engineer@gmail.com>
2023-06-15 17:34:47 -0400

    structure(root): add fake grid to barrel file

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -3,5 +3,6 @@ export { default as InfiniteScroll } from "./InfiniteScroll";
 export { default as LazyImage } from "./LazyImage";
 export { default as Logo } from "./Logo";
 export { default as Card } from "./placeholders/Card";
+export { default as FakeGrid } from "./placeholders/FakeGrid";
 export { default as Lines } from "./placeholders/Lines";
 export { default as Page } from "./placeholders/Page";
