929c367b1da8dc854909928ea03fe249481a82b9
Brayan Forero <bforero.engineer@gmail.com>
2023-06-15 17:38:23 -0400

    refactor(root): change error message on useTerms hook

--- src/hooks/useTrendingSearches.ts
+++ src/hooks/useTrendingSearches.ts
@@ -1,31 +1,31 @@
-import { useEffect, useState } from 'react'
-import { getTrendingSearches } from '../services/trending'
+import { useEffect, useState } from "react";
+import { getTrendingSearches } from "../services/trending";
 
 function useTrendingSearches() {
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState<string | null>(null)
-  const [terms, setTerms] = useState<string[]>([])
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [terms, setTerms] = useState<string[]>([]);
 
   useEffect(() => {
-    setError(null)
-    setLoading(true)
+    setError(null);
+    setLoading(true);
     getTrendingSearches()
-      .then(data => {
-        if (data) setTerms(data.data)
+      .then((data) => {
+        if (data) setTerms(data.data);
       })
-      .catch(_err => {
-        setError('Fetching data error')
+      .catch((_err) => {
+        setError("Something has been wrong");
       })
       .finally(() => {
-        setLoading(false)
-      })
-  }, [])
+        setLoading(false);
+      });
+  }, []);
 
   return {
     loading,
     terms,
     error,
-  }
+  };
 }
 
-export default useTrendingSearches
+export default useTrendingSearches;
