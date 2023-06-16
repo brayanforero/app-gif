3a89141efce82df94ed8a48efc84f1316b1ae05e
Brayan Forero <bforero.engineer@gmail.com>
2023-06-16 12:44:16 -0400

    ui(root): fix layouts shift terms

--- src/components/shared/placeholders/Lines.tsx
+++ src/components/shared/placeholders/Lines.tsx
@@ -1,26 +1,18 @@
-import ContentLoader from 'react-content-loader'
+import ContentLoader from "react-content-loader";
 
 function Lines() {
   return (
     <ContentLoader
       speed={2}
-      width={340}
-      height={80}
-      viewBox="0 0 340 80"
+      width={`100%`}
+      height={60}
+      viewBox="0 0 100% 60"
       backgroundColor="#373d48"
       foregroundColor="#282c34"
     >
-      <rect x="0" y="0" rx="3" ry="3" width="67" height="11" />
-      <rect x="76" y="0" rx="3" ry="3" width="140" height="11" />
-      <rect x="127" y="48" rx="3" ry="3" width="53" height="11" />
-
-      <rect x="187" y="48" rx="3" ry="3" width="72" height="11" />
-      <rect x="18" y="48" rx="3" ry="3" width="100" height="11" />
-      <rect x="0" y="71" rx="3" ry="3" width="37" height="11" />
-      <rect x="18" y="23" rx="3" ry="3" width="140" height="11" />
-      <rect x="166" y="23" rx="3" ry="3" width="173" height="11" />
+      <rect x="20%" y="0" rx="4" ry="4" width="60%" height="100%" />
     </ContentLoader>
-  )
+  );
 }
 
-export default Lines
+export default Lines;
