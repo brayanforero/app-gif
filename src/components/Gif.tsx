aa5b37d80deee566e1dff049d2888ddae6db360d
Brayan Forero <bforero.engineer@gmail.com>
2023-06-18 16:49:51 -0400

    ui(root): change styles gif component

--- src/components/Gif.tsx
+++ src/components/Gif.tsx
@@ -1,35 +1,34 @@
-import { memo } from 'react'
-import GifModel from '../interfaces/app/gif'
-
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-import 'react-lazy-load-image-component/src/effects/blur.css'
-import './Gif.css'
-import { Link } from 'wouter'
+import { memo } from "react";
+import GifModel from "../interfaces/app/gif";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "./Gif.css";
+import { Link } from "wouter";
+import { LazyImage } from "./shared";
 interface Props {
-  data: GifModel
-  type?: 'hd' | 'normal'
+  data: GifModel;
+  type?: "hd" | "normal";
 }
 
-function Gif({ data, type = 'normal' }: Props) {
+function Gif({ data, type = "normal" }: Props) {
   return (
-    <div className="Gif">
-      <LazyLoadImage
-        decoding="async"
-        effect="blur"
-        useIntersectionObserver={true}
-        src={type === 'normal' ? data.image.normal : data.image.hd}
+    <article className="Gif">
+      <LazyImage
+        width={300}
+        height={300}
+        src={type === "normal" ? data.image.normal : data.image.hd}
         alt={data.name}
-        width="100%"
-        height="100%"
       />
-      <Link to={`/g/${data.id}`} className="Gif__name">
-        {data.name || 'Title Unavaliable'}
-      </Link>
-    </div>
-  )
+
+      <h2>
+        <Link to={`/g/${data.id}`} className="Gif__name">
+          {data.name || "Title Unavaliable"}
+        </Link>
+      </h2>
+    </article>
+  );
 }
 
 export default memo(
   Gif,
   (prevProps, nextProps) => prevProps.data.id === nextProps.data.id
-)
+);
