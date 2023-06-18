4392228d12ee4c81590bec59ba4c48f0294ded7f
Brayan Forero <bforero.engineer@gmail.com>
2023-06-18 15:39:35 -0400

    fix(root): layouts shitf when terms has loaded

--- src/components/shared/placeholders/Lines.tsx
+++ src/components/shared/placeholders/Lines.tsx
@@ -5,12 +5,12 @@ function Lines() {
     <ContentLoader
       speed={2}
       width={`100%`}
-      height={60}
-      viewBox="0 0 100% 60"
+      height={140}
+      viewBox="0 0 100% 140"
       backgroundColor="#373d48"
       foregroundColor="#282c34"
     >
-      <rect x="20%" y="0" rx="4" ry="4" width="60%" height="100%" />
+      <rect x="0" y="0" rx="4" ry="4" width="100%" height="100%" />
     </ContentLoader>
   );
 }
