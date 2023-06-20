7f521dea78ddc34b820419a4ed5f7b26270a2966
Brayan Forero <bforero.engineer@gmail.com>
2023-06-20 10:40:17 -0400

    fix(root): size fake lines

--- src/components/common/home/HomeFeed/TrendingTerms.tsx
+++ src/components/common/home/HomeFeed/TrendingTerms.tsx
@@ -1,10 +1,11 @@
+import { Lines } from "@components/shared";
 import TermsList from "./TermsList";
 import useTrendingSearches from "@hooks/useTrendingSearches";
 function TrendingTerms() {
-  const { terms, error } = useTrendingSearches();
+  const { terms, error, loading } = useTrendingSearches();
 
   if (error) return <span className="alert">{error}</span>;
-
+  if (loading) return <Lines />;
   return (
     <section className="Home__searches">
       <TermsList terms={terms} />
