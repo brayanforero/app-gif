7a9761240f24e078c5a5bb4c7c2977914ff4e072
Brayan Forero <bforero.engineer@gmail.com>
2023-06-20 10:07:08 -0400

    fix(root): scroll restoration

--- src/components/shared/ScrollReset/index.tsx
+++ src/components/shared/ScrollReset/index.tsx
@@ -1,9 +1,18 @@
-import { PropsWithChildren, useEffect } from "react";
+import { PropsWithChildren, useEffect, useRef } from "react";
+import { useLocation, useRouter } from "wouter";
 
 function ScrollReset({ children }: PropsWithChildren) {
+  const [location] = useLocation();
+  const rootRef = useRef<HTMLDivElement | null>(null);
+
   useEffect(() => {
-    window.scrollTo(0, 0);
-  });
+    rootRef.current = document.querySelector("#root");
+  }, []);
+
+  useEffect(() => {
+    if (rootRef.current == null) return;
+    rootRef.current.scroll(0, 0);
+  }, [location]);
 
   return children;
 }
--- src/components/shared/SearchBar/index.tsx
+++ src/components/shared/SearchBar/index.tsx
@@ -1,12 +1,24 @@
 import { Container } from "@components/layouts";
 import Form from "../Form";
 import Logo from "../Logo";
+import { useLocation } from "wouter";
 import "./index.css";
+
 function SearchBar() {
+  const [, to] = useLocation();
+
+  const handleNavigate = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+
+    to("/");
+  };
+
   return (
     <header className="SearchBar">
       <Container>
-        <Logo />
+        <a onClick={handleNavigate}>
+          <Logo />
+        </a>
         <Form />
       </Container>
     </header>
